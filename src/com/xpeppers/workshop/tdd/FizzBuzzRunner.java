9007e83f88871f44a40ab9b057eb7550657a2f10
lorisugolini <loris.ugolini@gmail.com>
2016-11-21 11:00:46 +0100

    handled range of numbers

--- src/com/xpeppers/workshop/tdd/FizzBuzzRunner.java
+++ src/com/xpeppers/workshop/tdd/FizzBuzzRunner.java
@@ -1,5 +1,7 @@
 package com.xpeppers.workshop.tdd;
 
+import java.util.StringJoiner;
+
 public class FizzBuzzRunner {
 
 	private FizzBuzz fizzBuzz;
@@ -8,8 +10,12 @@ public class FizzBuzzRunner {
 		fizzBuzz = new FizzBuzz();
 	}
 
-	public String runOn(int first, int second) {
-		return fizzBuzz.runOn(first) + ", " + fizzBuzz.runOn(second);
+	public String runOn(int lowerBound, int upperBound) {
+		StringJoiner resultCollector = new StringJoiner(", ");
+		for (int i = lowerBound; i <= upperBound; i++) {
+			resultCollector.add(fizzBuzz.runOn(i));
+		}
+		return resultCollector.toString();
 	}
 
 }
--- test/com/xpeppers/workshop/tdd/FizzBuzzRunnerTest.java
+++ test/com/xpeppers/workshop/tdd/FizzBuzzRunnerTest.java
@@ -2,13 +2,27 @@ package com.xpeppers.workshop.tdd;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class FizzBuzzRunnerTest {
 
+	private FizzBuzzRunner fizzBuzzRunner;
+
+	@Before
+	public void buildRunner() {
+		fizzBuzzRunner = new FizzBuzzRunner();
+	}
+
 	@Test
-	public void runOnTwoNumbers() {
-		assertEquals("1, 2", new FizzBuzzRunner().runOn(1, 2));
+	public void runOnARangeOfThreeNumbers() {
+		assertEquals("1, 2, Fizz", fizzBuzzRunner.runOn(1, 3));
+	}
+
+	@Test
+	public void runOnARangeOfManyNumbers() {
+		assertEquals("1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz, 16",
+				fizzBuzzRunner.runOn(1, 16));
 	}
 
 }
